fd028d7ac661536da17d78974887d600503a1e52
Hyundong Hwang <hhd2002@gmail.com>
2016-06-08 13:43:23 +0900

    first commit

---
+++ MyDb/create-tables.sql
@@ -0,0 +1,37 @@
+﻿SELECT @@VERSION
+SELECT @@SERVERNAME
+EXEC sp_databases
+
+USE tempdb
+
+IF (EXISTS(SELECT name 
+FROM master.dbo.sysdatabases
+WHERE name = 'mydb'))
+    BEGIN
+    PRINT 'mydb is existed!'
+    DROP DATABASE mydb
+    END
+ELSE
+    PRINT 'mydb is not existed!'
+
+CREATE DATABASE mydb
+USE mydb
+SELECT DB_NAME()
+
+CREATE TABLE [dbo].[Course] (
+    [CourseID]  INT           IDENTITY (1, 1) NOT NULL,
+    [Title]     NVARCHAR (50) NULL,
+    [Credits]   INT           NULL,
+    [Credits2]  INT           NULL,
+    PRIMARY KEY CLUSTERED ([CourseID] ASC)
+)
+
+CREATE TABLE [dbo].[Student] (
+    [StudentID]      INT           IDENTITY (1, 1) NOT NULL,
+    [LastName]       NVARCHAR (50) NULL,
+    [FirstName]      NVARCHAR (50) NULL,
+    [EnrollmentDate] DATETIME      NULL,
+    PRIMARY KEY CLUSTERED ([StudentID] ASC)
+)
+
+SELECT * FROM information_schema.tables
---
+++ MyEfDbFirstClientTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEfDbFirstClientTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new MyDbContext())
+            {
+                context.Database.Log += (log) => 
+                {
+                    Console.WriteLine($"log : {log}");
+                };
+
+                context.Students.Add(new Student
+                {
+                    LastName = "황",
+                    FirstName = "현동",
+                    EnrollmentDate = DateTime.Now
+                });
+
+                context.SaveChanges();
+
+                var list = context.Students.ToList();
+            }
+        }
+    }
+}
